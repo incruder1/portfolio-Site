175e0ca2d4696dff24cfcd66a3b6015296c31d8f
Dev Johri <56020041+incruder1@users.noreply.github.com>
2023-09-02 05:21:54 +0530

    Initial commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Dev Johri </span>
+            from <span className="purple"> Ghaziabad,Uttar Pradesh, India.</span>
+            <br /> I am a final year student pursuing an Integrated BTech IT and MBA at ABV-IIITM Gwalior.
+            <br />
+            {/* Additionally, I am currently searching as a software developer at
+            Juspay. */}
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Reading History
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Embrace the challenges of today's code, for they are the stepping stones to tomorrow's innovation!"{" "}
+          </p>
+          <footer className="blockquote-footer">anonymous </footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import leaf from "../../Assets/Projects/6.png"; 
+import editor from "../../Assets/Projects/5.png";
+import ecommerce from "../../Assets/Projects/1.png"; 
+import two from "../../Assets/Projects/2.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={ecommerce}
+              isBlog={false}
+              title="Full Stack Ecommerce Website"
+              description=" MERN-based Ecommerce Website with an Admin Panel, offering users an intuitive shopping experience. Features include user authentication, product management, and secure payments. The admin panel allows easy item addition, editing, and deletion."
+              ghLink="https://github.com/incruder1/ecommerce-fullStack"
+              demoLink="https://easy-gray-bear-belt.cyclic.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={two}
+              isBlog={false}
+              title="Youtube Bookmark"
+              description="Chrome extension for streamlined YouTube video bookmarking. It simplifies the process of saving and managing favorite videos, enhancing user browsing experience. Developed with JavaScript"
+              ghLink="https://github.com/incruder1/Youtube-Bookmark-Chrome-Extension"
+             
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={editor}
+              isBlog={false}
+              title="VideoChatApp-WebRTC"
+              description="I designed a real-time video calling application using Socket.IO and WebRTC, enabling seamless video communication.Providing users with a reliable platform for virtual meetings and collaboration."
+              ghLink="https://github.com/incruder1/VideoChatApp-WebRTC.git"
+                            
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={leaf}
+              isBlog={false}
+              title="FrontEnd Ecommerce Website-Strapi"
+              description="creating responsive, visually appealing online shopping platforms. It emphasizes user experience, intuitive navigation, and dynamic product displays, delivering an engaging and efficient retail interface.."
+              ghLink="https://github.com/incruder1/Ecommerce-App-frontend-/tree/main"
+              
+            />
+          </Col>
+
+           
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
